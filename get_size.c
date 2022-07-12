1936c72ce11dc240d519b35d1cf4235d4c06a7a1
Ajao Malik <ajaomalik2018@gmail.com>
2022-07-12 09:17:18 -0700

    get_size.c

--- get_size.c
+++ get_size.c
@@ -3,6 +3,7 @@
 /**
  * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which is to print the arguments
+ *
  * @i: List of arguments to be printed.
  *
  * Return: Precision.
